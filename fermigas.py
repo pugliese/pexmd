00a1a0d8f3008d9baeb3cd6445a5f70304fdc9fd
pugliese <facu_pugliese@hotmail.com>
2018-09-27 21:30:36 -0300

    Intento de fermigas con potencial central en lugar de paredes

--- fermigas.py
+++ fermigas.py
@@ -18,6 +18,11 @@ def particulas(Npart,L):
 def energia_cinetica(v, m):
   return 0.5*sum(sum(v**2))*m[0]
 
+def fuerzas_caja(x, L, k):
+  fuerzas = k*(0.5*L - x)
+  epot = 0.5*k*sum(sum((0.5*L - x)**2))
+  return fuerzas, epot
+
 def avanzarRK(parts, pauli, integ, bx, therm):
   parts.f, e = pauli.forces(parts.x, parts.p)
   gorces, e = pauli.gorces(parts.x, parts.p)
@@ -31,28 +36,30 @@ def avanzarRK(parts, pauli, integ, bx, therm):
 
 def avanzar(parts, pauli, integ, bx, therm):
   parts.f, e = pauli.forces(parts.x, parts.p)
+  force_aux, ecaja = fuerzas_caja(parts.x, 4, 10)
   gorces, e = pauli.gorces(parts.x, parts.p)
-  parts.x, parts.v = integ.step(parts.x, parts.v, gorces, parts.a)
-  parts.x, parts.v = bx.wrap_boundary(parts.x, parts.v)
+  parts.x, parts.v = integ.step(parts.x, parts.v, gorces, parts.a + force_aux/parts.mass[0])
+  #parts.x, parts.v = bx.wrap_boundary(parts.x, parts.v)
   parts.v = therm.step(parts.v, parts.mass)
-  return parts.x, parts.v, e
+  rmax = max([sum((parts.x[i,:]-2)**2) for i in range(len(parts.x[:,0]))])
+  return parts.x, parts.v, e, ecaja, np.sqrt(rmax)
 
 # Interaction
 qo = 1.664
 po = 120
 h_barra = 196.727394
-D = 207*(h_barra/(po*qo))**3
+D = -207*(h_barra/(po*qo))**3
 scut = 20
 pauli = pexmd.interaction.Pauli(scut, D, qo, po)
 # Box
-L = .5*qo
+L = 4#5*qo
 bx = pexmd.box.Box(np.zeros(3), L*np.ones(3), 'Fixed')
 # Thermostat
 Q = 0#0.05
 To = .5
 therm = pexmd.thermostat.Andersen(To, Q)
 # Particles
-Npart = 3
+Npart = 30
 m = 0.5109989461
 parts = pexmd.particles.PointParticles(Npart)
 parts.x = particulas(Npart, L)
@@ -60,22 +67,25 @@ parts.v = np.random.normal(0,np.sqrt(To*1.5), (Npart,3))
 parts.mass = m
 # Integrator
 h = 0.00005
-integ = pexmd.integrator.RK2(h)
+integ = pexmd.integrator.Euler(h)
 
 Nterm = 100000
 Epot = np.zeros(Nterm)
+Epotcaja = np.zeros(Nterm)
 Ecin = np.zeros(Nterm)
+rmax = np.zeros(Nterm)
 for i in range(Nterm):
   if (i%1000==0):
     print(i)
-  parts.x, parts.v, Epot[i] = avanzarRK(parts, pauli, integ, bx, therm)
+  parts.x, parts.v, Epot[i], Epotcaja[i], rmax[i] = avanzar(parts, pauli, integ, bx, therm)
   Ecin[i] = energia_cinetica(parts.v, parts.mass)
 T = Ecin/(1.5*Npart)
 
 plt.figure()
-plt.plot(T)
+plt.plot(rmax)
 plt.figure()
 plt.plot(Ecin, 'r')
 plt.plot(Epot, 'b')
-plt.plot(Ecin+Epot, 'g')
+plt.plot(Epotcaja, 'b--')
+plt.plot(Ecin+Epot+Epotcaja, 'g')
 plt.show()
