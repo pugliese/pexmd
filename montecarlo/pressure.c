6c76cc1b3694098ef863b86ff271d85828b192fc
pugliese <facu_pugliese@hotmail.com>
2019-01-31 00:20:19 -0300

    Agregado calculo del termino de presion de QCNM

--- montecarlo/pressure.c
+++ montecarlo/pressure.c
@@ -3,6 +3,60 @@
 #include <time.h>
 #include <unistd.h>
 
+
+float pair_force_qcnm(float r, float Vo, float r1, float p1,
+                  float r2, float p2, float d, float a){
+
+  double qexp = exp((r-d)/a);
+  double r_red1_pot = pow(r1/r, p1);
+  double r_red2_pot = pow(r2/r, p2);
+  return Vo*( (p1*r_red1_pot - p2*r_red2_pot)/r + (r_red1_pot - r_red2_pot)*qexp/(a*(1+qexp)) )/((1+qexp)*r);
+
+}
+
+float pressure_qcnm_layers(float *x, long int* pairs, long int npairs, float Vo, float r1,
+      float p1, float r2, float p2, float d, float a, float rcut, float *force, float L, int ls){
+
+  float pres = 0.0;
+  for(int e1 = -ls; e1 <= ls; e1++){
+    for(int e2 = -ls; e2 <= ls; e2++){
+      for(int e3 = -ls; e3 <= ls; e3++){
+
+        float celda[3];
+        celda[0] = L*e1;
+        celda[1] = L*e2;
+        celda[2] = L*e3;
+
+        for (int l = 0; l < npairs; l++) {
+          float delta_r[3];
+          int i = pairs[2*l];
+          int j = pairs[2*l+1];
+
+          for (int k = 0; k < 3; k++) {
+            delta_r[k] = x[3*i+k] + celda[0] - x[3*j+k];
+          }
+
+          float r = 0;
+          for (int k = 0; k < 3; k++) {
+            r = r + delta_r[k]*delta_r[k];
+          }
+          r = sqrt(r);
+
+          if (r<rcut) {
+            float m_force = pair_force_qcnm(r, Vo, r1, p1, r2, p2, d, a);
+            for (int k = 0; k < 3; k++){
+              pres += delta_r[k] * m_force * delta_r[k];
+              force[3*i+k] += m_force*delta_r[k];
+              force[3*j+k] -= m_force*delta_r[k];
+            }
+          }
+        }
+      }
+    }
+  }
+  return pres;
+}
+
 float pressure_lj_PBC(float *x, long int* pairs, long int npairs, float eps,
              float sigma, float rcut, float *force, float L) {
   float pres = 0.0;
--- montecarlo/pressure.h
+++ montecarlo/pressure.h
@@ -5,6 +5,10 @@
 float pressure_lj_PBC(float *x, long int* pairs, long int npairs, float eps,
              float sigma, float rcut, float *force, float L);
 
+float pressure_qcnm_layers(float *x, long int* pairs, long int npairs, float Vo, float r1,
+     float p1, float r2, float p2, float d, float a, float rcut, float *force, float L, int ls);
+float pair_force_qcnm(float r, float Vo, float r1, float p1, float r2, float p2, float d, float a);
+
 float pair_force(float s2, float D, float qo2);
 float pair_gorce(float s2, float D, float po2);
 float pressure_pauli_PBC(float *x, float *p, long int* pairs, long int npairs,
